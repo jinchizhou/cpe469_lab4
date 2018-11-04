dadc024d653e1047d0886b71717a6b4da1a1c465
James Nishijima <david1@Macintosh-2.local>
2018-11-04 13:39:18 -0800

    hello jinchi

--- init.go
+++ init.go
@@ -4,6 +4,25 @@ import (
 	"fmt"
 )
 
-func main(){
-  fmt.Println("starts");
+type Node struct {
+	prev_index int
+	next_index int
+	index int
+	status bool
 }
+
+func main(){
+	tables := make([][]bool, 8)
+	for i := 0; i < 8; i++ {
+		tables[i] = make([]bool, 8)
+	}
+	fmt.Println("starts");
+	for i := 0; i < 8; i++ {
+		for j := 0; j < 8; j++ {
+			fmt.Print(tables[i][j])
+		}
+		fmt.Print("\n")
+	}
+}
+
+
