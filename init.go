2791dc78871ec999713f0fe10efdebe53fef0516
Jinchi Zhou <jinchizhou@yahoo.com>
2018-11-05 12:34:41 -0800

    works for sequential

--- init.go
+++ init.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
   "time"
+  // "sync"
 )
 
 type Node struct {
@@ -22,13 +23,11 @@ func print_table(tables [][]bool){
 }
 
 // checks if at least one node is alive
-func alive(tables [][]bool) bool{
+func alive(cand []Node) bool{
   for i := 0; i < 8; i++ {
-		for j := 0; j < 8; j++ {
-			if tables[i][j]{
+		if (cand[i].status){
         return true
-      }
-		}
+    }
 	}
   return false
 }
@@ -38,6 +37,39 @@ func print_Node(nodes []Node){
   for i := 0; i < 8; i++ {
     fmt.Println("Candidate # %s, prev_index: %s, next_index: %s, curr_index: %s, status: ", i, nodes[i].prev_index, nodes[i].next_index, nodes[i].index, nodes[i].status)
   }
+}
+// kills one Node and updates status
+func killOneNode(live_table [][]bool, candidates []Node){
+	for i:=0; i < 8; i++{
+		if (candidates[i].status){
+			// kill it, update statuses
+			live_table[i][i] = false
+			candidates[i].status = false
+			return
+		}
+	}
+}
+func exchangeTables(i int, live_table [][]bool, candidates []Node, connector []int){
+	// assume if status of candidate is false, it is dead, so return
+	if (!candidates[i].status){
+		return
+	}
+	p_index := candidates[i].prev_index
+	n_index := candidates[i].next_index
+	// if previous is dead
+	if (!candidates[p_index].status){
+		connector[0] = i
+	}
+	// if next is dead
+	if (!candidates[n_index].status){
+		connector[1] = i;
+	}
+	// loop through prev_index and update on live_table
+	for j:=0; j < 8; j++{
+		if(!live_table[p_index][j]){
+			live_table[i][j] = false
+		}
+	}
 }
 func main(){
   // initialize global table with every node's status
@@ -107,19 +139,40 @@ func main(){
   time_ex_table := time.Now()
   // every 5 sec, one node dies
   time_node := time.Now()
-  for alive(tables){
-    // exchange tables
-    if (time.Since(time_ex_table).Seconds() > 0.2){
-
-      fmt.Println(time.Since(time_ex_table).Seconds())
-      time_ex_table = time.Now()
-    }
+  // var wg sync.WaitGroup
+  for alive(candidates){
     // one node dies
-    if (time.Since(time_node).Seconds() > 5){
-
+    if (time.Since(time_node).Seconds() > 2){
+      killOneNode(tables, candidates)
       fmt.Println("One dies ", time.Since(time_node).Seconds())
       time_node = time.Now()
     }
+    // exchange tables
+    if (time.Since(time_ex_table).Seconds() > 0.2){
+      // connect two if one died
+      connector := make([]int, 2)
+      connector[0] = -1
+      connector[1] = -1
+      // exchange tables
+      for i:= 0; i < 8; i++{
+        // go func(){
+        //   wg.Add(1)
+        //   exchangeTables(i, tables, candidates, connector)
+        //   defer wg.Done()
+        // }()
+        exchangeTables(i, tables, candidates, connector)
+      }
+      // wg.Wait()
+      if (connector[0] != -1){
+        // one node died, so connect connector[0] to connector[1]
+        candidates[connector[0]].prev_index = connector[1]
+        candidates[connector[1]].next_index = connector[0]
+      }
+      print_table(tables)
+      fmt.Println(time.Since(time_ex_table).Seconds())
+      time_ex_table = time.Now()
+    }
   }
+  // done
 
 }
