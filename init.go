6854be3da23ecf7f610a47e5290b931421eddb1d
James Nishijima <david1@Macintosh-2.local>
2018-11-06 17:32:33 -0800

    spacing

--- init.go
+++ init.go
@@ -1,34 +1,34 @@
 package main
 
 import (
-	"fmt"
+  "fmt"
   "time"
   "sync"
 )
 
 type Node struct {
-	prev_index int
-	next_index int
-	index int
-	status bool
+  prev_index int
+  next_index int
+  index int
+  status bool
 }
 
 func print_table(tables [][]bool){
   for i := 0; i < 8; i++ {
-		for j := 0; j < 8; j++ {
-			fmt.Print(" ", tables[i][j])
-		}
-		fmt.Print("\n")
-	}
+    for j := 0; j < 8; j++ {
+      fmt.Print(" ", tables[i][j])
+    }
+    fmt.Print("\n")
+  }
 }
 
 // checks if at least one node is alive
 func alive(cand []Node) bool{
   for i := 0; i < 8; i++ {
-		if (cand[i].status){
-        return true
+    if (cand[i].status){
+      return true
     }
-	}
+  }
   return false
 }
 
@@ -40,57 +40,57 @@ func print_Node(nodes []Node){
 }
 // kills one Node and updates status
 func killOneNode(live_table [][]bool, candidates []Node){
-	for i:=0; i < 8; i++{
-		if (candidates[i].status){
-			// kill it, update statuses
-			live_table[i][i] = false
-			candidates[i].status = false
-			return
-		}
-	}
+  for i:=0; i < 8; i++{
+    if (candidates[i].status){
+      // kill it, update statuses
+      live_table[i][i] = false
+      candidates[i].status = false
+      return
+    }
+  }
 }
 func exchangeTables(i int, live_table [][]bool, candidates []Node, connector []int, wg *sync.WaitGroup, mutex *sync.RWMutex){
   defer wg.Done()
   mutex.Lock()
   // assume if status of candidate is false, it is dead, so return
-	if (!candidates[i].status){
+  if (!candidates[i].status){
     mutex.Unlock()
-		return
-	}
-	p_index := candidates[i].prev_index
-	n_index := candidates[i].next_index
-	// if previous is dead
-	if (!candidates[p_index].status){
-		connector[0] = i
-	}
-	// if next is dead
-	if (!candidates[n_index].status){
-		connector[1] = i;
-	}
-	// loop through prev_index and update on live_table
-	for j:=0; j < 8; j++{
-		if(!live_table[p_index][j]){
+    return
+  }
+  p_index := candidates[i].prev_index
+  n_index := candidates[i].next_index
+  // if previous is dead
+  if (!candidates[p_index].status){
+    connector[0] = i
+  }
+  // if next is dead
+  if (!candidates[n_index].status){
+    connector[1] = i;
+  }
+  // loop through prev_index and update on live_table
+  for j:=0; j < 8; j++{
+    if(!live_table[p_index][j]){
       live_table[i][j] = false
-		}
+    }
   }
   mutex.Unlock()
 }
 func main(){
   // initialize global table with every node's status
-	tables := make([][]bool, 8)
-	for i := 0; i < 8; i++ {
-		tables[i] = make([]bool, 8)
+  tables := make([][]bool, 8)
+  for i := 0; i < 8; i++ {
+    tables[i] = make([]bool, 8)
     for j := 0; j < 8; j++ {
       tables[i][j] = true
     }
-	}
+  }
   // init a list of node's global status
   candidates := []Node{
       {
         prev_index: 7,
-      	next_index: 1,
-      	index: 0,
-      	status: true,
+        next_index: 1,
+        index: 0,
+        status: true,
       },
       {
         prev_index: 0,
